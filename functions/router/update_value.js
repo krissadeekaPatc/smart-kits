e09200dd24a8977e035cc3f2f5d6caccfcd4d022
Krissadeeka <krissadeeka.patc@gmail.com>
2021-04-21 09:58:39 +0700

    [DEV] Develop isLight function [done]

--- functions/router/update_value.js
+++ functions/router/update_value.js
@@ -45,7 +45,7 @@ async function getDeviceData(req, res) {
           uid: req.headers.uid,
           uniqueName: req.headers.uname,
           devicename: element.devicename,
-
+          isLight: "true",
           automations: [
             {
               module: "switch1",
@@ -174,4 +174,37 @@ async function sendDataChart(req, res) {
 }
 
 router.post("/chart", async (req, res) => await sendDataChart(req, res));
+
+async function toggleLight(req, res) {
+  try {
+    const client = await connect();
+    if (!client) {
+      return res.send("CANNOT CONNECT TO DATABASE");
+    } else {
+      const db = client.db(dbName);
+
+      let sensorCollection = db.collection("sensors");
+
+      const myquery = {
+        uid: req.headers.uid,
+        devicename: req.headers.devicename,
+      };
+      var newvalues = {
+        $set: {
+          isLight: req.body.isLight,
+        },
+      };
+
+      sensorCollection.updateMany(myquery, newvalues, function (err, result) {
+        if (err) throw err;
+        res.status(200).send(result);
+      });
+    }
+  } catch (e) {
+    console.log("GET IN CATCH: " + e);
+    res.send("ERROR :" + e);
+  }
+}
+
+router.patch("/toggleLight", async (req, res) => await toggleLight(req, res));
 module.exports = router;
